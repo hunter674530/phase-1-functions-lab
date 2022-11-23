bb29531622c864b38f0bdb3ef66797eb2cda45d6
Hunter Wood <hunter674530@gmail.com>
2022-11-23 10:54:02 -0600

    commit tree 44add481439537dbb6b4343f709cddb01b34fdff

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  if (block > 42) {
+    return block - 42;
+  } else return 42 - block;
+}
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  if (start > destination) {
+    return (start - destination) * 264;
+  } else {
+    return (destination - start) * 264;
+  }
+}
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    let price = 0;
+    return price;
+  } else if (distance > 400 && distance <= 2000) {
+    let price = (distance - 400) * 0.02;
+    return price;
+  } else if (distance > 2000 && distance <= 2500) {
+    let price = 25;
+    return price;
+  } else {
+    return "cannot travel that far";
+  }
+}
